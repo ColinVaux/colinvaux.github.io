271bf6581d0833357cbd90d5e1436b82e6de0ca4
ColinVaux <125408994+ColinVaux@users.noreply.github.com>
2023-04-17 11:11:47 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     const nav = document.querySelector('.nav');
     const trigger = document.querySelector('.trigger');
     const container = document.querySelectorAll('.container');
-    let over = 0;
+
     if (window.innerWidth < window.innerHeight) {
         container.forEach(function(each) {
             each.setAttribute('class', 'portrait');
             });
         };
+
     window.addEventListener('resize', function() {
         if (window.innerWidth < window.innerHeight) {
             container.forEach(function(each) {
                 each.setAttribute('class', 'portrait');
             });
         };
+        
         if (window.innerWidth > window.innerHeight) {
             container.forEach(function(each) {
                 each.setAttribute('class', 'landscape');
             });
         };
     });
+
     document.querySelectorAll('.nav-link').forEach(function(each) {
        const check = each.getAttribute('href');
        const page = document.URL;
@@ -27,14 +31,26 @@ document.addEventListener('DOMContentLoaded', function() {
            each.setAttribute('class', 'nav-link active disabled');
            each.setAttribute('aria-current', 'page');
            document.title = `${each.innerHTML}`;
-    };
+        };
     });
+
     trigger.addEventListener('mouseenter', function() {
         trigger.setAttribute('class', 'trigger sticky-bottom curser')
     });
+
     trigger.addEventListener('mouseleave', function() {
         trigger.setAttribute('class', 'trigger sticky-bottom')
     });
+
+    document.addEventListener('scroll', function() {
+        nav.setAttribute('class', 'nav justify-content-center')
+        setTimeout(function() {
+            if (trigger.getAttribute('class').includes('curser') == false) {
+                nav.setAttribute('class', 'nav justify-content-center hidden')
+            };
+          }, 2000);
+    });
+
     trigger.addEventListener('mouseenter', function() {
         setTimeout(function() {
             if (trigger.getAttribute('class').includes('curser') == true) {
@@ -42,6 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
             };
           }, 500);
     });
+
     nav.addEventListener('mouseleave', function() {
         setTimeout(function() {
             if (trigger.getAttribute('class') == 'trigger sticky-bottom') {
